ffb5138fdba22c74d5be0eb195607b6fee113a19
Juan Manuel <juanm.bquintana@gmail.com>
2019-03-13 22:49:41 +0100

    [library-spring] update to spring boot 2.2.0M1

--- library-infrastructure/src/main/java/com/dodecaedro/library/infrastructure/controller/BookController.java
+++ library-infrastructure/src/main/java/com/dodecaedro/library/infrastructure/controller/BookController.java
@@ -1,7 +1,6 @@
 package com.dodecaedro.library.infrastructure.controller;
 
 import com.dodecaedro.library.domain.model.Book;
-import com.dodecaedro.library.domain.repository.BookRepository;
 import com.dodecaedro.library.infrastructure.repository.ElasticBookSearchDocumentRepository;
 import com.dodecaedro.library.infrastructure.repository.JpaBookRepository;
 import com.dodecaedro.library.infrastructure.search.BookSearchDocument;
@@ -10,19 +9,19 @@ import org.springframework.cache.annotation.CachePut;
 import org.springframework.data.rest.webmvc.PersistentEntityResource;
 import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
-import org.springframework.hateoas.Resources;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 
 @RepositoryRestController
 @RequestMapping(path = "books")
@@ -46,7 +45,7 @@ public class BookController {
 	@CachePut(value = "books", key = "#p0.Id")
 	public PersistentEntityResource saveBook(@RequestBody Book book, PersistentEntityResourceAssembler resourceAssembler) {
 		searchDocumentRepository.save(bookToSearchDocumentConverter.toSearchDocument(book));
-		return resourceAssembler.toResource(bookRepository.save(book));
+		return resourceAssembler.toModel(bookRepository.save(book));
 	}
 
 	@GetMapping("search")
@@ -57,7 +56,7 @@ public class BookController {
 			.collect(Collectors.toList());
 
 		var books = bookIds.isEmpty() ? Collections.emptyList() : bookRepository.findAllById(bookIds);
-		var resources = new Resources<>(books);
+		var resources = new CollectionModel<>(books);
 		resources.add(linkTo(methodOn(BookController.class).findBooksByAttributes(title)).withSelfRel());
 		return ResponseEntity.ok(resources);
 	}
--- library-infrastructure/src/main/java/com/dodecaedro/library/infrastructure/controller/BorrowController.java
+++ library-infrastructure/src/main/java/com/dodecaedro/library/infrastructure/controller/BorrowController.java
@@ -6,7 +6,7 @@ import com.dodecaedro.library.domain.exception.ExpiredBorrowException;
 import com.dodecaedro.library.domain.model.Borrow;
 import com.dodecaedro.library.domain.service.BorrowService;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
-import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,8 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RepositoryRestController
 public class BorrowController {
@@ -29,7 +29,7 @@ public class BorrowController {
 	public @ResponseBody ResponseEntity<?> borrowBook(@RequestBody Borrow borrow)
 		throws ExpiredBorrowException, ActiveFinesException, BorrowMaximumLimitException {
 		var createdBorrow = borrowService.borrowBook(borrow.getBook(), borrow.getUser());
-		var resource = new Resource<>(createdBorrow);
+		var resource = new EntityModel<>(createdBorrow);
 		resource.add(linkTo(methodOn(BorrowController.class).borrowBook(borrow)).withSelfRel());
 		return ResponseEntity.status(HttpStatus.CREATED).body(resource);
 	}
