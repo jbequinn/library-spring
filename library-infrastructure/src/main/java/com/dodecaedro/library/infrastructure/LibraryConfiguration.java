79e10a4ee5927606f56780b951deb584f52545ad
Juan Manuel <jbequinn@gmail.com>
2019-07-01 21:06:57 +0200

    [library-spring] refactor application configuration annotations

--- library-infrastructure/src/main/java/com/dodecaedro/library/infrastructure/LibraryConfiguration.java
+++ library-infrastructure/src/main/java/com/dodecaedro/library/infrastructure/LibraryConfiguration.java
@@ -5,10 +5,25 @@ import com.dodecaedro.library.domain.repository.BorrowRepository;
 import com.dodecaedro.library.domain.repository.FineRepository;
 import com.dodecaedro.library.domain.repository.UserRepository;
 import com.dodecaedro.library.domain.service.BorrowService;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.config.BootstrapMode;
 
 @Configuration
+@EnableJpaRepositories(
+		bootstrapMode = BootstrapMode.DEFERRED,
+		basePackages = "com.dodecaedro.library.infrastructure.repository"
+)
+@EnableElasticsearchRepositories(
+		basePackages = "com.dodecaedro.library.infrastructure.repository"
+)
+@EntityScan("com.dodecaedro.library.domain.model")
+@EnableCaching
 public class LibraryConfiguration {
 	@Bean
 	BorrowService borrowService(LibraryConfigurationProperties libraryConfiguration, FineRepository fineRepository,
--- library-infrastructure/src/main/java/com/dodecaedro/library/infrastructure/LibraryInfrastructureApplication.java
+++ library-infrastructure/src/main/java/com/dodecaedro/library/infrastructure/LibraryInfrastructureApplication.java
@@ -2,22 +2,8 @@ package com.dodecaedro.library.infrastructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.repository.config.BootstrapMode;
 
 @SpringBootApplication
-@EnableJpaRepositories(
-	bootstrapMode = BootstrapMode.DEFERRED,
-	basePackages = "com.dodecaedro.library.infrastructure.repository"
-)
-@EnableElasticsearchRepositories(
-	basePackages = "com.dodecaedro.library.infrastructure.repository"
-)
-@EntityScan("com.dodecaedro.library.domain.model")
-@EnableCaching
 public class LibraryInfrastructureApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryInfrastructureApplication.class, args);
