2d5df0ee5ffd611c38989bfebd3b577ef63a5cca
Juan Manuel <jbequinn@gmail.com>
2020-09-22 18:54:05 +0200

    [library-spring] add gradle plugins for coveralls with cobertura

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
 pluginManagement {
 	repositories {
 		maven { url = uri("https://repo.spring.io/milestone") }
+		maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
 		gradlePluginPortal()
 	}
 	resolutionStrategy {
