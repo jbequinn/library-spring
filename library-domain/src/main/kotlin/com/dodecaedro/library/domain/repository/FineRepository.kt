27a2156eb45850d61e3db6eca9a8aec102143994
Juan Manuel <jbequinn@gmail.com>
2020-11-13 16:32:33 +0100

    [library-spring] convert domain repository interfaces to kotlin

---
+++ library-domain/src/main/kotlin/com/dodecaedro/library/domain/repository/FineRepository.kt
@@ -0,0 +1,17 @@
+package com.dodecaedro.library.domain.repository
+
+import com.dodecaedro.library.domain.model.Borrow
+import com.dodecaedro.library.domain.model.Fine
+import com.dodecaedro.library.domain.model.User
+import java.time.ZonedDateTime
+import java.util.*
+
+interface FineRepository {
+	fun findTopByUserOrderByFineEndDateDesc(user: User): Optional<Fine>
+
+	fun save(fine: Fine): Fine
+
+	fun findActiveFinesInDate(user: User, time: ZonedDateTime): List<Fine>
+
+	fun findByBorrow(borrow: Borrow): Optional<Fine>
+}
