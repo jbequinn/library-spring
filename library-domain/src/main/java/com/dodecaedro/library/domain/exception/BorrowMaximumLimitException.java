8d1d0a6827d42c81b95d62b27d5109cc8f766cd5
Juan Manuel <jbequinn@gmail.com>
2020-11-13 16:07:44 +0100

    [library-spring] convert exceptions to kotlin

--- library-domain/src/main/java/com/dodecaedro/library/domain/exception/BorrowMaximumLimitException.java
+++
@@ -1,7 +0,0 @@
-package com.dodecaedro.library.domain.exception;
-
-public class BorrowMaximumLimitException extends Exception {
-	public BorrowMaximumLimitException(String message) {
-		super(message);
-	}
-}
---
+++ library-domain/src/main/kotlin/com/dodecaedro/library/domain/exception/BorrowMaximumLimitException.kt
@@ -0,0 +1,3 @@
+package com.dodecaedro.library.domain.exception
+
+class BorrowMaximumLimitException(message: String) : Exception(message)
